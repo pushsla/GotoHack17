8c3c5758cb5e2b088227d33579821b5f9fd9a17d
DmitryInd <dmitry.indenbom@yandex.ru>
2017-06-21 19:42:26 +0300

    Up

--- telegramm (1).py
+++ telegramm (1).py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(token=token)
 
 users = {}
 
-truth = [True, True, False, False, False, False, False]
+truth = [True, True, False, False, False, False, False, False, False, False, False, False, False, False, False]
 
 i = 0
 string = '0'
@@ -45,6 +45,8 @@ def write_data(name):
         fil.write(st)
     fil.close()
 
+x = False
+
 @bot.message_handler(commands=["start"])
 def DIRECTION(message):
     if truth[0] == True:
@@ -52,30 +54,15 @@ def DIRECTION(message):
         bot.send_message(message.chat.id, "Хочешь сыграть в киллера?  Да/Нет")
         truth[0] = False
         
-def exactly(message, string, f, i):
-    print (f)
-    bot.send_message(message.chat.id, "Подтверди...")
-    while f == '0':
-        f = message.text
-    print (f)
-    if f != string:
-        bot.send_message(message.chat.id, "Не совпадают, повторим операцию...")
-        truth[i] = False
-        truth[i-1] = True
-    else: 
-        truth[i] = False
-        truth[i + 1] = True
-    return f
-
 @bot.message_handler(content_types=["text"])
 def start(message):
-    global string, i
-    if truth[1] == True and (message.text == "Да" or i == 1):
+    global string, i, x
+    if truth[1] == True and (message.text.lower() == "да" or i == 1):
         bot.send_message(message.chat.id, "Здравствуй, у меня есть для тебя заказ. Сообщи свои данные чтобы я мог назначить тебя подальше от врагов.")
-        bot.send_message(message.chat.id, "Назови своё имя:")
+        bot.send_message(message.chat.id, "Назови своё имя (английскими буквами):")
         truth[1] = False
         truth[2] = True
-    elif truth[1] == True and message.text == "Нет":
+    elif truth[1] == True and message.text.lower() == "нет":
         bot.send_message(message.chat.id, "Заходи в другой раз, каждый день в 14:00.")
     elif truth[2] == True:
         string = message.text
@@ -83,13 +70,19 @@ def start(message):
         f = '0'
         i = 2
         truth[2] = False
-        truth[5] = True
+        truth[6] = True
     elif truth[3] == True:
         add_gamer(string)
         bot.send_message(message.chat.id, "Номер комнаты:")
         truth[3] = False
         truth[4] = True
     elif truth[4] == True:
+        string = message.text
+        bot.send_message(message.chat.id, "Подтверди:")
+        truth[4] = False
+        truth[6] = True
+        i = 4
+    elif truth[5] == True:
         keyboard = types.InlineKeyboardMarkup()
         callback_button0 = types.InlineKeyboardButton(text="РОБОТОТЕХНИКА", callback_data="1")
         callback_button1 = types.InlineKeyboardButton(text="ПРИКЛАДНОЕ ПРОГРАММИРОВАНИЕ", callback_data="2")
@@ -100,33 +93,97 @@ def start(message):
         keyboard.add(callback_button2)
         keyboard.add(callback_button3)
         bot.send_message(message.chat.id, "ВАШЕ НАПРАВЛЕНИЕ:", reply_markup=keyboard)
-    elif truth[5] == True:
+    elif truth[6] == True:
         f = message.text
         print(f)
         if f != string:
             bot.send_message(message.chat.id, "Не совпадают, повтори операцию...", "\n", "Нажмите на любую кнопку (+ enter)...")
             i -= 1
+            truth[6] = False
             truth[i] = True
-        else: 
+        else:
+            truth[6] = False
             i += 1 
             truth[i] = True
             bot.send_message(message.chat.id, "Нажмите на любую кнопку (+ enter)...")
-    
-
+            x = True
+    elif truth[7] == True:
+        keyboard = types.InlineKeyboardMarkup()
+        callback_button0 = types.InlineKeyboardButton(text="Да", callback_data="1")
+        callback_button1 = types.InlineKeyboardButton(text="Нет", callback_data="2")
+        keyboard.add(callback_button0)
+        keyboard.add(callback_button1)
+        bot.send_message(message.chat.id, "Играешь в настольный теннис?:", reply_markup=keyboard)
+        truth[7] = False
+        truth[4] = True
+        bot.send_message(message.chat.id, "Нажмите любую кнопку (+enter)")
+    elif truth[8] == True:
+        keyboard = types.InlineKeyboardMarkup()
+        callback_button0 = types.InlineKeyboardButton(text="Да", callback_data="1")
+        callback_button1 = types.InlineKeyboardButton(text="Нет", callback_data="2")
+        keyboard.add(callback_button0)
+        keyboard.add(callback_button1)
+        bot.send_message(message.chat.id, "Играешь в футбол?", reply_markup=keyboard)
+        truth[8] = False
+        truth[3] = True
+        bot.send_message(message.chat.id, "Нажмите любую кнопку (+enter)")
+    elif truth[9] == True:
+        keyboard = types.InlineKeyboardMarkup()
+        callback_button0 = types.InlineKeyboardButton(text="Да", callback_data="1")
+        callback_button1 = types.InlineKeyboardButton(text="Нет", callback_data="2")
+        keyboard.add(callback_button0)
+        keyboard.add(callback_button1)
+        bot.send_message(message.chat.id, "Играешь в волейбол?", reply_markup=keyboard)
+        truth[9] = False
+        truth[2] = True
+        bot.send_message(message.chat.id, "Нажмите любую кнопку (+enter)")
+        
 @bot.callback_query_handler(func=lambda call: True)
 def CALL(call):
-    if call.message:
-        if call.data == "1":
-            D = 1
-            print(D)
-        if call.data == "2":
-            D = 2
-            print(D)
-        if call.data == "3":
-            D = 3
-            print(D)
-        if call.data == "4":
-            D = 4
-            print(D)
-            
+    if truth[5] == True:
+        if call.message:
+            if call.data == "1":
+                D = 1
+                print(D)
+            if call.data == "2":
+                D = 2
+                print(D)
+            if call.data == "3":
+                D = 3
+                print(D)
+            if call.data == "4":
+                D = 4
+                print(D)
+        truth[5] = False
+        truth[7] = True
+    elif   truth[4] == True:
+        if call.message:
+            if call.data == "1":
+                D = 1
+                print(D)
+            if call.data == "2":
+                D = 2
+                print(D)
+        truth[4] = False
+        truth[8] = True
+    elif truth [3] == True:
+        if call.message:
+            if call.data == "1":
+                D = 1
+                print(D)
+            if call.data == "2":
+                D = 2
+                print(D)
+        truth[3] = False
+        truth[9] = True
+    elif truth [2] == True:
+        if call.message:
+            if call.data == "1":
+                D = 1
+                print(D)
+            if call.data == "2":
+                D = 2
+                print(D)
+        truth[2] = False
+        truth[0] = 
 bot.polling(none_stop=True)
